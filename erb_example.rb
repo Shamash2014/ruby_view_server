df29e23bcc2abef7929ea437bf83f59a98578f1b
Shamash <warp.buddhist@gmail.coml>
2015-04-21 13:25:16 +0300

    exercise 1 committes

--- erb_example.rb
+++ erb_example.rb
@@ -2,4 +2,19 @@ require 'erb'
 
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
+
+study_hours = []
+5.times do |x|
+	x += 3
+	study_hours << x
+end
+my_meal = {pizza: 'Colmon', drink: "Water", alcohol: "Whiskey"}
+
+y = "I have a study plan"
+zTemp = ERB.new "<h1><%= y %></h1>"
+pTemp = ERB.new "<p>Overall time will be <%= study_hours.inject{|sum, n| sum +n} %> hours</p>"
+iTemp = ERB.new "I like pizza <%= my_meal[:pizza] %> and drink <%= my_meal[:alcohol]%>"
 puts template.result(binding)
+puts pTemp.result()
+puts zTemp.result()
+puts iTemp.result()
